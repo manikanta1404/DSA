f6020b4bcbfb9d04553e006f611747a662726c64
Manikanta <manikantachowdary1404@gmail.com>
2025-05-07 09:13:19 +0530

    Create patterns.java

---
+++ Day-03/patterns.java
@@ -0,0 +1,119 @@
+/* Online Java Compiler and Editor */
+public class HelloWorld{
+
+     public static void main(String []args){
+        System.out.println("mani");
+        
+        // for(int i=1;i<=5;i++){
+        //     for(int j=1;j<=5;j++){
+        //         System.out.printf("*");
+        //     }
+        //     System.out.println();
+        // }
+        
+        
+        // for (int i=1;i<=5;i++){
+        //     for(int j=1;j<=5;j++){
+        //         if(i==1 || i==5|| j==1 ||j==5 ){
+        //             System.out.print("*");
+        //         }
+        //         else{
+        //             System.out.print(" ");
+        //         }
+                
+        //     }
+        //     System.out.println();
+        // }
+        
+        // for (int i=1;i<=5;i++){
+        //     for(int j=1;j<=5;j++){
+        //         if(i==1 || i==5|| j==1 ||j==5 ||i==j){
+        //             System.out.print("* ");
+        //         }
+        //         else{
+        //             System.out.print("  ");
+        //         }
+                
+        //     }
+        //     System.out.println();
+        
+        // }
+        
+        // for (int i=1;i<=5;i++){
+        //     for(int j=1;j<=5;j++){
+        //         if(i==1 || i==5|| j==1 ||j==5 ||i==j ||i+j==6){
+        //             System.out.print("* ");
+        //         }
+        //         else{
+        //             System.out.print("  ");
+        //         }
+                
+        //     }
+        //     System.out.println();
+        // }
+        
+        // for (int i=1;i<=5;i++){
+        //     for(int j=1;j<=5;j++){
+        //         if( i==5||j==1 ||j==5 ||i==j ||i+j==6){
+        //             if(i<4){
+        //                 System.out.print("* ");
+        //             }
+        //         }
+        //         else{
+        //             System.out.print("  ");
+        //         }
+                
+        //     }
+        //     System.out.println();
+        // }
+        
+        
+        // for (int i=1;i<=5;i++){
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print("* ");
+        //         }
+        //     System.out.println();
+        // }
+        
+        // for(int i=1;i<=5;i++){
+        //     for(int j=1;j<=5;j++){
+        //         if(j==1||i==j||i==5){
+        //             System.out.print("* ");
+        //         }
+        //         else{
+        //             System.out.print("  ");
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+        
+        //  for(int i=1;i<=5;i++){
+        //     for(int j=1;j<=5;j++){
+        //         if(j==1||i==j||j==5){
+        //             System.out.print("* ");
+        //         }
+        //         else{
+        //             System.out.print("  ");
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+        
+        // for(int i=1;i<=5;i++){
+        //     for(int j=1;j<=5;j++){
+        //         if(j==5||i+j==6||i==5){
+        //             System.out.print("* ");
+        //         }
+        //         else{
+        //             System.out.print("  ");
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+        
+        
+     }
+}
+
+
+
