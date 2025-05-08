c4ab91f1b3773db5c23a48d4d4f7184e3aad1580
Manikanta <manikantachowdary1404@gmail.com>
2025-05-08 10:12:26 +0530

    Update patterns.java

--- Day-03/patterns.java
+++ Day-03/patterns.java
@@ -176,7 +176,44 @@ public class HelloWorld{
         //       1234567
         //      123456789
         // }
+
+
+                for(int i=1;i<=5;i++){
+            for(int s=5;s>=i;s--){
+                    System.out.print("  ");
+                }
+                for(int j=1;j<=2*i-1;j++){
+                
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
         
+        for(int i=5;i>=1;i--){
+            for(int s=5;s>=i;s--){
+                    System.out.print("  ");
+                }
+                for(int j=1;j<=2*i-1;j++){
+                
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+        
+  //       {
+  //           output
+            
+  //         1 
+  //       1 2 3 
+  //     1 2 3 4 5 
+  //   1 2 3 4 5 6 7 
+  // 1 2 3 4 5 6 7 8 9 
+  // 1 2 3 4 5 6 7 8 9 
+  //   1 2 3 4 5 6 7 
+  //     1 2 3 4 5 
+  //       1 2 3 
+  //         1 
+  //       }
         
      }
 }
