7c7a54c958c2ab7dd1a9f0562aab15579bf51d3f
Manikanta <manikantachowdary1404@gmail.com>
2025-05-09 10:22:29 +0530

    Create sorted.java

---
+++ Day-05/sorted.java
@@ -0,0 +1,22 @@
+// check weather the array is sorted or not
+
+
+
+
+// check weather the given element is there or not
+
+
+
+
+
+// in a given arrays we have only 1's and 0's
+
+
+
+
+
+
+
+
+
+// move all 0's to right side 
